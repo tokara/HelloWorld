35f4c340f20fed42b2a3330a179e01eb14487ff0
User <User@194.47.44.94>
2016-04-14 08:57:20 +0200

    new commit

---
+++ src/helloworld/Helloworld.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author User
+ */
+public class Helloworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World");// TODO code application logic here
+    }
+    
+}
